0818450974d0739b1cc957359896c9adadaa0cea
Mark Lee <mlcy0508@gmail.com>
2024-08-02 13:48:51 +0800

    update keymap and add support for snaptap

--- config.h
+++ config.h
@@ -31,11 +31,10 @@ for more options.
 /* ws2812 RGB LED */
 #define WS2812_DI_PIN D3
 
-#ifdef RGB_MATRIX_ENABLE
-#define RGBLED_NUM 36  // Number of LEDs
-#define RGBLED_NUM 36  // Number of LEDs
-#define RGB_MATRIX_LED_COUNT RGBLED_NUM
-#endif
+// #ifdef RGB_MATRIX_ENABLE
+// #define RGBLED_NUM 36  // Number of LEDs
+// #define RGB_MATRIX_LED_COUNT RGBLED_NUM
+// #endif
 
 #ifdef RGBLIGHT_ENABLE
 #undef RGBLED_NUM
@@ -52,10 +51,10 @@ for more options.
 // #define RGBLIGHT_EFFECT_ALTERNATING
 // #define RGBLIGHT_EFFECT_TWINKLE
 
-#define RGBLED_NUM 70
+// #define RGBLED_NUM 70
 // #define RGBLED_SPLIT
-#define RGBLED_SPLIT \
-    { 35, 35 }  // haven't figured out how to use this yet
+#undef RGBLED_SPLIT
+#define RGBLED_SPLIT { 35, 35 }  // haven't figured out how to use this yet
 
 // #define RGBLED_NUM 30
 #define RGBLIGHT_LIMIT_VAL 100
