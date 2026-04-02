cf9e52ca9328d37950466800b521b1a9074b71ea
Mark Lee <mlcy0508@gmail.com>
2026-04-03 03:15:14 +0800

    remove rgb and enable oled

--- keymap.c
+++ keymap.c
@@ -1,28 +1,8 @@
+#pragma once
 #include <stdio.h>
-
 #include QMK_KEYBOARD_H
 
-#define INDICATOR_BRIGHTNESS 20
-
-#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
-#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
-
-// Light combinations
-#define SET_UNDERGLOW(hsv) \
-    {0, 6, hsv}, \
-    {35, 6,hsv}
-#define SET_LAYER_ID(hsv) \
-    {0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {1, 6, hsv}, \
-    {35+1, 6, hsv}, \
-    {7, 4, hsv}, \
-    {35+ 7, 4, hsv}, \
-    {25, 2, hsv}, \
-    {35+ 25, 2, hsv}
-
 enum sofle_layers {
-    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _DEFAULTS = 0,
     _COLEMAK = 0,
     _QWERTY,
@@ -36,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_COLN, KC_QUOT, KC_LSFT, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_RSFT, KC_LCTL, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_MPLY, KC_TAB, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_LALT, KC_LGUI, KC_SPC, MO(2), KC_BSPC, KC_ENT, MO(3), KC_ENT, MO(5), KC_RALT),
     [1] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RSFT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, KC_TAB, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_LALT, KC_LGUI, KC_SPC, MO(2), KC_BSPC, KC_ENT, MO(3), KC_SPC, MO(5), KC_RALT),
     [2] = LAYOUT(KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PIPE, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO, KC_LSFT, KC_NO, KC_BSLS, KC_UNDS, KC_MINS, KC_TILD, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_RSFT, KC_LCTL, KC_NO, KC_SLSH, KC_EQL, KC_PLUS, KC_GRV, KC_MUTE, KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS),
-    [3] = LAYOUT(KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_NO, KC_SCLN, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_DQUO, KC_NO, KC_LBRC, KC_RBRC, KC_NO, KC_NO, KC_RSFT, KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_QUOT, KC_TRNS, KC_TRNS, KC_NO, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [3] = LAYOUT(KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_NO, KC_SCLN, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_DQUO, KC_NO, KC_LBRC, KC_RBRC, KC_NO, KC_NO, KC_RSFT, KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_QUOT, KC_TRNS, KC_TRNS, KC_NO, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [4] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, DF(0), DF(1), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_F12, KC_NO, UG_VALD, UG_VALU, UG_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [5] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, KC_NO, KC_NO, MS_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F12, KC_NO, KC_NO, MS_LEFT, MS_DOWN, MS_RGHT, KC_NO, KC_NO, MS_BTN1, MS_BTN2, MS_BTN3, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MS_BTN4, MS_BTN5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_TRNS)
 };
@@ -46,70 +26,10 @@ socd_cleaner_t socd_opposing_pairs[] = {
   {{KC_A, KC_D}, SOCD_CLEANER_LAST},
 };
 
-#ifdef RGBLIGHT_ENABLE
-char layer_state_str[70];
-// Now define the array of layers. Later layers take precedence
-
-// QWERTY,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_UNDERGLOW(HSV_ORANGE)
-);
-
-const rgblight_segment_t PROGMEM layer_colemak_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_UNDERGLOW(HSV_PINK)
-);
-
-// _UPPER,
-// Light on outer column and underglow
-const rgblight_segment_t PROGMEM layer_upper_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    SET_UNDERGLOW(HSV_TEAL)
-);
-
-// _LOWER,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_lower_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    SET_UNDERGLOW(HSV_BLUE)
-);
-
-// _FUNCTION,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_function_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    SET_UNDERGLOW(HSV_PURPLE)
-);
-
-// _MOUSE   // light up top row
-const rgblight_segment_t PROGMEM layer_mouse_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    SET_UNDERGLOW(HSV_GREEN)
-);
-
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-
-    layer_colemak_lights,
-    layer_qwerty_lights,
-    layer_lower_lights,
-    layer_upper_lights,
-    layer_function_lights,
-    layer_mouse_lights
-);
-
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_COLEMAK));
-    rgblight_set_layer_state(1, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY) && IS_LAYER_ON_STATE(state, _QWERTY));
-
-    rgblight_set_layer_state(2, layer_state_cmp(state, _LOWER));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _UPPER));
-    rgblight_set_layer_state(4, layer_state_cmp(state, _FUNCTION));
-    rgblight_set_layer_state(5, layer_state_cmp(state, _MOUSE));
+    IS_LAYER_ON_STATE(state, _QWERTY);
     return state;
 }
-void keyboard_post_init_user(void) {
-    // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
-
-    rgblight_mode(10);// haven't found a way to set this in a more useful way
-}
-#endif
 
 #ifdef OLED_ENABLE
 
@@ -126,20 +46,12 @@ static void render_logo(void) {
 static void print_status_narrow(void) {
     // Print current mode
     oled_write_P(PSTR("\n\n"), false);
-    oled_write_ln_P(PSTR("MODE"), false);
-    oled_write_ln_P(PSTR(""), false);
-    // if (keymap_config.swap_lctl_lgui) {
-    //     oled_write_ln_P(PSTR("MAC"), false);
-    // } else {
-    //     oled_write_ln_P(PSTR("WIN"), false);
-    // }
-    //
     switch (get_highest_layer(default_layer_state)) {
         case _QWERTY:
-            oled_write_ln_P(PSTR("Qwrt"), false);
+            oled_write_ln_P(PSTR("M:QTY"), false);
             break;
         case _COLEMAK:
-            oled_write_ln_P(PSTR("Clmk"), false);
+            oled_write_ln_P(PSTR("M:CMK"), false);
             break;
         default:
             oled_write_P(PSTR("Undef"), false);
@@ -170,8 +82,6 @@ static void print_status_narrow(void) {
             oled_write_ln_P(PSTR("Undef"), false);
     }
     oled_write_P(PSTR("\n\n"), false);
-    led_t led_usb_state = host_keyboard_led_state();
-    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
